00a7f4b00f2f0b47254a00ed791eafca235bf137
Luis Oliveira <dev.oliveiras@gmail.com>
2020-02-01 11:45:26 -0300

    Created Item Data assets

--- Assets/Scripts/Data/ItemData.cs
+++ Assets/Scripts/Data/ItemData.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ItemData : MonoBehaviour
+[CreateAssetMenu(menuName ="Data/Item")]
+public class ItemData : ScriptableObject
 {
     public int id;
     public Sprite sprite;
