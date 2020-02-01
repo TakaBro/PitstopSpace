e3863f2a81cdc94acfa9b71bfa78c53923913bf6
Luis Oliveira <dev.oliveiras@gmail.com>
2020-02-01 10:36:02 -0300

    Created Item component and Data

--- Assets/Scripts/Components/ItemComponent.cs
+++ Assets/Scripts/Components/ItemComponent.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class ItemComponent : MonoBehaviour
 {
-    [SerializeField] ItemData data;
+    [SerializeField] ItemData itemData;
+
+    private void Start()
+    {
+        if (itemData)
+        {
+            GetComponent<SpriteRenderer>().sprite = itemData.sprite;
+        }
+    }
+    public void SetData(ItemData data)
+    {
+        itemData = data;
+        GetComponent<SpriteRenderer>().sprite = itemData.sprite;
+    }
 }
