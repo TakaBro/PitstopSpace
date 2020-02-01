f7257a0a8e8d545f224dac0751928660de04e5e0
Lukas Takatani <lekt@icomp.ufam.edu.br>
2020-02-01 13:04:38 -0300

    Spaceship Data and Component

---
+++ Assets/Scripts/Components/SpaceshipComponent.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceshipComponent : MonoBehaviour
+{
+    [SerializeField] SpaceshipData spaceshipData;
+
+    private void Start()
+    {
+        if (spaceshipData)
+        {
+            GetComponent<SpriteRenderer>().sprite = spaceshipData.sprite;
+        }
+    }
+    public void SetData(SpaceshipData data)
+    {
+        spaceshipData = data;
+        GetComponent<SpriteRenderer>().sprite = spaceshipData.sprite;
+    }
+}
---
+++ Assets/Scripts/Data/SpaceshipData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+[CreateAssetMenu(menuName = "Data/Spaceship" )]
+public class SpaceshipData : ScriptableObject
+{
+    //public float height;
+    public List<Slots> slots;
+    public Sprite sprite;
+}
+
+[Serializable]
+public class Slots
+{
+    public int id;
+    public bool enable;
+}
