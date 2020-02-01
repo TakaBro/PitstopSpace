7327e7f76e50f5b1992b3df104d92b54ff962209
Luis Oliveira <dev.oliveiras@gmail.com>
2020-02-01 18:14:27 -0300

    Finished implementing Conveyor Belt Mechanics and animation

--- Assets/Scripts/Components/ConveyorBeltComponent.cs
+++ Assets/Scripts/Components/ConveyorBeltComponent.cs
@@ -5,11 +5,26 @@ using UnityEngine.Events;
 
 public class ConveyorBeltComponent : MonoBehaviour
 {
+    [SerializeField] ItemsManager itemsManager;
     [SerializeField] ConveyorBeltData data;
     [SerializeField] ItemComponent itemComponent;
+    [SerializeField] Transform itemContainer;
     [SerializeField] UnityEvent onTimeReached;
-    List<ItemComponent> items;
+    [SerializeField] List<ItemComponent> items;
+
     float timer;
+    
+    Dictionary<Vector2, Vector2> positionPairs;
+    private void Start()
+    {
+        InitializePositionPairs();
+        foreach (Vector2 position in data.positions)
+        {
+            ItemComponent item = Instantiate(itemComponent, itemContainer);
+            item.Position = position;
+            items.Add(item);
+        }
+    }
 
     public void Update()
     {
@@ -17,6 +32,23 @@ public class ConveyorBeltComponent : MonoBehaviour
         {
             Debug.Log("Move");
             onTimeReached.Invoke();
+            MoveItems();
+        }
+    }
+
+    public void InitializePositionPairs()
+    {
+        positionPairs = new Dictionary<Vector2, Vector2>();
+        for (int i = 0; i < data.positions.Count; i++)
+        {
+            if (i+1 == data.positions.Count)
+            {
+                positionPairs.Add(data.positions[i], data.positions[0]);
+            }
+            else
+            {
+                positionPairs.Add(data.positions[i], data.positions[i+1]);
+            }
         }
     }
 
@@ -31,6 +63,28 @@ public class ConveyorBeltComponent : MonoBehaviour
         {
             timer = 0;
             return true;
+        }
+    }
+
+    void MoveItems()
+    {
+        foreach (ItemComponent item in items)
+        {
+            Vector2 target;
+            positionPairs.TryGetValue(item.Position, out target);
+            item.MoveTo(target, data.moveDelay);
+            if (item.Position == data.LastPosition())
+            {
+                item.Enable(false);
+                if (!item.IsFilled)
+                {
+                    item.SetData(itemsManager.GetRandomItem());
+                }
+            }
+            if (item.Position == data.positions[0])
+            {
+                item.Enable(true);
+            }
         }
     }
 }
