f6cd3137decc8a35ebe2c65dcaa7cfc55791ce4c
Lukas Takatani <lekt@icomp.ufam.edu.br>
2020-02-01 03:07:17 -0300

    PlayerMovement

---
+++ Assets/MovementController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementController : MonoBehaviour
+{
+
+    bool hasMoved;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.position += new Vector3(0.0f, 0.1f);
+        }else if(Input.GetKeyDown(KeyCode.DownArrow)){
+            transform.position += new Vector3(0.0f, -0.1f);
+        }
+
+        /*Debug.Log(Input.GetAxis("Vertical"));
+
+        if(Input.GetAxis("Vertical") == 0)
+        {
+            hasMoved = false;
+        }
+        else if(Input.GetAxis("Vertical") != 0 && !hasMoved)
+        {
+            hasMoved = true;
+
+            Move();
+        }*/
+    }
+
+    // Update is called once per frame
+    void Move()
+    {
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            transform.position += new Vector3(0.0f, -0.1f);
+        }else {
+            transform.position += new Vector3(0.0f, 0.1f);
+        }
+    }
+}
