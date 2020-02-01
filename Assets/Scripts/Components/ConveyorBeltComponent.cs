3ca7ef5799882b92e1152cf859853e4e4ff961d5
Luis Oliveira <dev.oliveiras@gmail.com>
2020-02-01 16:26:36 -0300

    Conveyor Belt now animates on predefined Time

---
+++ Assets/Scripts/Components/ConveyorBeltComponent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ConveyorBeltComponent : MonoBehaviour
+{
+    [SerializeField] ConveyorBeltData data;
+    [SerializeField] ItemComponent itemComponent;
+    [SerializeField] UnityEvent onTimeReached;
+    List<ItemComponent> items;
+    float timer;
+
+    public void Update()
+    {
+        if (TimeReached(data.moveDelay))
+        {
+            Debug.Log("Move");
+            onTimeReached.Invoke();
+        }
+    }
+
+    private bool TimeReached(float delay)
+    {
+        timer += Time.deltaTime;
+        if (timer < delay)
+        {
+            return false;
+        }
+        else
+        {
+            timer = 0;
+            return true;
+        }
+    }
+}
