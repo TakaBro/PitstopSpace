9f20ea136d65620fbef16f25925e40328c2da807
Lukas Takatani <lekt@icomp.ufam.edu.br>
2020-02-01 16:09:58 -0300

    Menu buttons call correct scenes WIP

---
+++ Assets/Scripts/UI/BtnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BtnClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BtnPlay()
+    {
+        SceneManager.LoadScene("Scenery 1");
+    }
+
+    public void BtnRanking()
+    {
+        //TO DO Ranking
+    }
+
+    // Update is called once per frame
+    public void BtnExit()
+    {
+        Application.Quit();
+    }
+}
